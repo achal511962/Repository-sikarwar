a26d7383e85368d38e3a67aef99bddafa30163d3
HP <HP@DESKTOP-99LKOE8>
2020-07-11 18:27:11 +0530

    Pro-Second commit.

---
+++ TestProject/src/com/test/java/TestMaster.java
@@ -0,0 +1,10 @@
+package com.test.java;
+
+public class TestMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("This is my first TestProject.");
+	}
+
+}
