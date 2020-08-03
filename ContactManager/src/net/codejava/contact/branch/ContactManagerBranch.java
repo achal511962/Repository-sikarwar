bcafec53eb6d25003a8aa94cd8563af8391601d5
HP <HP@DESKTOP-99LKOE8>
2020-08-03 14:09:26 +0530

    Added changes for NewBranchFromGitsSide-3Aug2020.

--- ContactManager/src/net/codejava/contact/branch/ContactManagerBranch.java
+++ ContactManager/src/net/codejava/contact/branch/ContactManagerBranch.java
@@ -2,4 +2,9 @@ package net.codejava.contact.branch;
 
 public class ContactManagerBranch {
      
+	private void sout() {
+		// TODO Auto-generated method stub
+     System.out.println("NewBranchFromGitsSide Testing - 3Aug2020.");
+	}
+
 }
