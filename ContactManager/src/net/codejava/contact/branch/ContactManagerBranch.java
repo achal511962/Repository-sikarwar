4d38aa24c5200f6474f10080ca50b8786cd32657
HP <HP@DESKTOP-99LKOE8>
2020-08-03 14:16:44 +0530

    Added new changes for 3Aug2020.

--- ContactManager/src/net/codejava/contact/branch/ContactManagerBranch.java
+++ ContactManager/src/net/codejava/contact/branch/ContactManagerBranch.java
@@ -5,6 +5,8 @@ public class ContactManagerBranch {
 	private void sout() {
 		// TODO Auto-generated method stub
      System.out.println("NewBranchFromGitsSide Testing - 3Aug2020.");
+     
+     System.out.println("NewBranchFromGitsSide Added in Last Time - 5Aug2020.");
 	}
 
 }
