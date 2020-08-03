e24da7f89a73f69d662277c288f4a831c7f6a8d7
HP <HP@DESKTOP-99LKOE8>
2020-08-03 12:48:38 +0530

    Added new changes NewBranch-7Apr20.

--- ContactManager/src/net/codejava/contact/ContactManagerBranch12.java
+++ ContactManager/src/net/codejava/contact/ContactManagerBranch12.java
@@ -1,5 +1,9 @@
 package net.codejava.contact;
 
 public class ContactManagerBranch12 {
-
+   
+	private void sout() {
+		// TODO Auto-generated method stub
+   System.out.println("Added in - NewBranch-7Apr20");
+	}
 }
