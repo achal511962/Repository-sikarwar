451b9f231033ebbd47eaffe09b8747e4d78e0814
HP <HP@DESKTOP-99LKOE8>
2020-10-11 17:49:46 +0530

    Added changes Branch_ABC30.

--- TestEcGitJenPro/src/BankAccount/Account.java
+++ TestEcGitJenPro/src/BankAccount/Account.java
@@ -2,7 +2,7 @@ package BankAccount;
 
 public class Account {
 	private  String number;  // accountNumber  SinghSikarwar_Branch_ABC20
-	private double balance;  // accountBalance
+	private double balance;  // accountBalance           Sikarwar-Branch-ABC30
 	private String customerName;
 	private String customerEmailaddress;
 	private String customerphone; 
