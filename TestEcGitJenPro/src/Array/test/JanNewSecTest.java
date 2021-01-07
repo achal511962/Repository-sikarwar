c76aa118b02ac7d7d05e704cbb5f88e440865b6b
HP <HP@DESKTOP-99LKOE8>
2021-01-07 21:48:05 +0530

    Added changes in new branch JanFutureProject02.

---
+++ TestEcGitJenPro/src/Array/test/JanNewSecTest.java
@@ -0,0 +1,10 @@
+package Array.test;
+
+public class JanNewSecTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("12Jab2021 Project Starts upadte02." );
+	}
+
+}
